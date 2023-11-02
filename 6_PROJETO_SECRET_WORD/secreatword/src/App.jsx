dfa9ef4e1ccf92596a9b552e67fa8ecca97788f7
3012056 <leno.moraes@ifmt.edu.br>
2023-11-02 12:42:54 -0400

    iniciando projeto do topico 6

---
+++ 6_PROJETO_SECRET_WORD/secreatword/src/App.jsx
@@ -0,0 +1,15 @@
+
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div className='base'>
+        <h1>Secrect Word</h1>
+      </div>      
+    </>
+  )
+}
+
+export default App
---
+++ 6_PROJETO_SECRET_WORD/secreatword/src/data/words.js
@@ -0,0 +1,8 @@
+export const wordsList = {
+    carro: ["Motor", "Porta", "Capo", "Pneu", "Antena"], 
+    fruta: ["Banana", "Maça", "Pêra", "Mamão", "Laranja"], 
+    corpo: ["Braço", "Perna", "cérebro", "pescoço", "olhos"],
+    computador: ["Mouse", "Teclado", "Monitor", "Gabinete"], 
+    programação: ["Linguagem", "Framework", "JavaScript", "React"],
+    alimento: ["Arnoz", "Feijão", "Carne", "Leite", "ovo"],
+};
